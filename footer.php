7310c1591128de992c76be63c2dddb182cb16204
Constanza Morales <coniconon@users.noreply.github.com>
2017-05-19 19:31:39 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+<p>Constanza Morales · Miércoles 17 de mayo, 2017 · <a href="https://github.com/coniconon/decima-clase/">Ver en repositorio Github</a></p>
+</footer>
+</div>
+<script src="libs/js/jquery.min.js"></script>
+<script src="libs/js/bootstrap.min.js"></script>
+</script>
+</body>
+</html>
---
+++ visualizacion.php
@@ -0,0 +1,8 @@
+<?php include('header.php');?>
+<section>
+<h2>Visualization of Global Gender Gap Index of 2013</h2>
+<iframe width="100%" height="520" frameborder="0" src="https://coniconon.carto.com/builder/38bbe240-3b6f-11e7-a87b-0e05a8b3e3d7/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+
+
+</section>
+<?php include('footer.php');?>
